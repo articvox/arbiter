8135a38f3549de1b948673e2c5d6207fae8d2b7e
Silver Adamson <silver.adamson@nortal.com>
2021-02-02 13:33:44 +0200

    wire mock ratings service

--- src/main/java/ee/adamson/arbiter/ArbiterApplication.java
+++ src/main/java/ee/adamson/arbiter/ArbiterApplication.java
@@ -1,15 +1,23 @@
 package ee.adamson.arbiter;
 
+import ee.adamson.arbiter.rating.control.MockRatingsService;
+import ee.adamson.arbiter.rating.control.RatingsService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class ArbiterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ArbiterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ArbiterApplication.class, args);
+    }
+
+    @Bean
+    public RatingsService ratingsService() {
+        return new MockRatingsService();
+    }
 
 }
